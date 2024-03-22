79dd3e5c5a35bc0a6026ef16aeb1088f40d99a0c
root <yono@redhat.com>
2024-03-22 18:14:14 +0900

    fix rhamq manifest

--- r4e/scripts/demo/0-image-build.sh
+++ r4e/scripts/demo/0-image-build.sh
@@ -7,6 +7,12 @@ else
 	VER=$1
 fi
 
+if [ -z "$2" ]; then
+	OSTREE_IP="localhost"
+else
+	OSTREE_IP=$1
+fi
+
 BLUEPRINT="rhde-microshift-${VER}"
 REFS="rhel/9/x86_64/edge"
 IMAGE_TYPE="edge-commit"
@@ -14,5 +20,5 @@ IMAGE_TYPE="edge-commit"
 if [ ${VER} = "1.0.0" ]; then
 	composer-cli compose start-ostree ${BLUEPRINT} ${IMAGE_TYPE}
 else
-	composer-cli compose start-ostree --url=http://10.0.2.12:9000/ostree/repo/ --ref=${REFS} --parent ${REFS} ${BLUEPRINT} ${IMAGE_TYPE}
+	composer-cli compose start-ostree --url=http://${OSTREE_IP}:9000/ostree/repo/ --ref=${REFS} --parent ${REFS} ${BLUEPRINT} ${IMAGE_TYPE}
 fi
