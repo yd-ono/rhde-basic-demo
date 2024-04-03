b6f7dd784b726f830febc0813ab9b8c18e498b5c
root <yono@redhat.com>
2024-04-03 17:05:49 +0900

    fix

--- dgw-deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AWX_TOKEN="Y6HCrWsFQW5D7tnReXlaLwXpgH5g6y"
-curl --insecure -X POST \
-	-H "Authorization: Bearer $AWX_TOKEN" \
-	-H "Content-Type: application/json" \
-	https://localhost/api/v2/job_templates/25/launch/
