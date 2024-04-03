5d246dc4ca1034092ac1c1a7f8216ec513179576
root <yono@redhat.com>
2024-04-03 16:27:42 +0900

    add AAP API scripts

---
+++ dgw-deployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+AWX_TOKEN="Y6HCrWsFQW5D7tnReXlaLwXpgH5g6y"
+curl --insecure -X POST \
+	-H "Authorization: Bearer $AWX_TOKEN" \
+	-H "Content-Type: application/json" \
+	https://localhost/api/v2/job_templates/25/launch/
