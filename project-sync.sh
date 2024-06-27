2c49d795b8f618f14eb238b4ae3109f921915966
yono1 <yono@redhat.com>
2024-06-27 11:28:31 +0900

    commit

--- project-sync.sh
+++ project-sync.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+git add .
+git commit -a -m $1
+git push
+
 AWX_TOKEN="Y6HCrWsFQW5D7tnReXlaLwXpgH5g6y"
 curl --insecure -X POST \
 	-H "Authorization: Bearer $AWX_TOKEN" \
 	-H "Content-Type: application/json" \
-	https://localhost/api/v2/projects/27/update/
+	https://localhost/api/v2/projects/8/update/
